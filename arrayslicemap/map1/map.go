9171bff0bf7eedfc28d5f58a2ca6b49f903e670b
Ricardo Oliveira <ricardo.oliveira@mindera.com>
2019-02-11 00:06:04 +0000

    array slice map

---
+++ arrayslicemap/map1/map.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var aprovados map[int]string
+	// mapas devem ser inicializados
+	aprovados := make(map[int]string)
+
+	aprovados[12345678978] = "Maria"
+	aprovados[21382913821] = "Pedro"
+	aprovados[38382139219] = "Ana"
+	fmt.Println(aprovados)
+
+	for cpf, nome := range aprovados {
+		fmt.Printf("%s (CPF: %d)\n", nome, cpf)
+	}
+
+	fmt.Println(aprovados[38382139219])
+	delete(aprovados, 38382139219)
+	fmt.Println(aprovados[38382139219])
+}
