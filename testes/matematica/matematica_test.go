093369ec12c390de44c539ea52579950047ce358
Ricardo Oliveira <ricardo.oliveira@mindera.com>
2019-02-18 12:00:18 +0000

    test

--- testes/matematica/matematica_test.go
+++ testes/matematica/matematica_test.go
@@ -5,6 +5,7 @@ import "testing"
 const erroPadrao = "Valor esperado %v, mas o resultado encontrado foi %v."
 
 func TestMedia(t *testing.T) {
+	t.Parallel()
 	valorEsperado := 7.28
 	valor := Media(7.2, 9.9, 6.1, 5.9)
 
@@ -15,3 +16,9 @@ func TestMedia(t *testing.T) {
 
 // Para executar todos os testes da pasta atual e subpastas:
 // go test ./...
+
+// Outros comandos de cobertura de tests:
+// go test -cover
+// go test -coverprofile=resultado.out
+// go tool cover -func=resultado.out
+// go tool cover -html=resultado.out
